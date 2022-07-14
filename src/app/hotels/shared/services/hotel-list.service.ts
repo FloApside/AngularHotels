3afc078790a0c87d8dabeaa8a46f6801bee9b02b
Flo <florian.desieter@apside-groupe.com>
2022-07-14 15:28:04 +0200

    140722.1528

--- src/app/hotels/shared/services/hotel-list.service.ts
+++ src/app/hotels/shared/services/hotel-list.service.ts
@@ -27,6 +27,7 @@ export class HotelListService {
     return this.getHotels().pipe(
       map((hotels) => hotels.find((hotel) => hotel.id == id))
       // tap(hotels => console.log("Hotel by id", hotels))
+      // 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
     );
   }
 
